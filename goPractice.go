cff86c0fd4dee37e6c1996a5c2eb856c09fb6a2d
@rima <32933855+arima0714@users.noreply.github.com>
2021-07-08 11:52:56 +0900

    コードの説明を追加

--- goPractice.go
+++ goPractice.go
@@ -6,6 +6,7 @@ import (
 )
 
 func main() {
+	// Write()メソッドで書き込まれた内容をためておいて、後でまとめて結果を受け取れるbytes.Buffer
 	var buffer bytes.Buffer
 	buffer.Write([]byte("bytes.Buffer example\n"))
 	fmt.Println(buffer.String())
