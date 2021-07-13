db6a6e6ec192d9c7cf94a25a706d73aa32a8b57a
@rima <32933855+arima0714@users.noreply.github.com>
2021-07-13 12:16:56 +0900

    エンディアンの変換をする関数のテストをした

--- goPractice.go
+++ goPractice.go
@@ -1,13 +1,16 @@
 package main
 
 import (
-	"io"
-	"os"
-	"strings"
+	"bytes"
+	"encoding/binary"
+	"fmt"
 )
 
 func main() {
-	reader := strings.NewReader("Example of io.SectionReader\n")
-	sectionReader := io.NewSectionReader(reader, 14, 7)
-	io.Copy(os.Stdout, sectionReader)
+	// 32ビットのビッグエンディアンのデータ(10000）
+	data := []byte{0x0, 0x0, 0x27, 0x10}
+	var i int32
+	// エンディアンの変換
+	binary.Read(bytes.NewReader(data), binary.BigEndian, &i)
+	fmt.Printf("data: %d\n", i)
 }
