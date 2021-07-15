4e09263a19bbffac5158067ebc84078b2eb0eab7
@rima <32933855+arima0714@users.noreply.github.com>
2021-07-15 12:19:12 +0900

    引数として渡されたファイル名を改行付きでひとつづつ表示するようにした

--- cat/cat.go
+++ cat/cat.go
@@ -7,9 +7,9 @@ import (
 
 func main() {
 	flag.Parse()
-	fmt.Println(flag.Args())
+	args := flag.Args()
 	// ファイルが存在しなければ「cat: <ファイル名>: No such file or directory」と表示
-	for argIndex := 0; argIndex < len(flag.Args()); argIndex++{
-		fmt.Println(argIndex)
+	for argIndex := 0; argIndex < len(args); argIndex++ {
+		fmt.Println(args[argIndex])
 	}
 }
