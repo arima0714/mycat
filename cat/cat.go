9dbc78da5db3ef84a869bbdc480d421db0ff8592
@rima <32933855+arima0714@users.noreply.github.com>
2021-07-15 12:17:44 +0900

    ファイル数だけ、数字を表示するようにした

--- cat/cat.go
+++ cat/cat.go
@@ -8,4 +8,8 @@ import (
 func main() {
 	flag.Parse()
 	fmt.Println(flag.Args())
+	// ファイルが存在しなければ「cat: <ファイル名>: No such file or directory」と表示
+	for argIndex := 0; argIndex < len(flag.Args()); argIndex++{
+		fmt.Println(argIndex)
+	}
 }
