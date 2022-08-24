0435d89b2ab2099a9aa8bba465cadd3a6a82169f
@rima <32933855+arima0714@users.noreply.github.com>
2022-08-25 06:45:28 +0900

    出力部分を関数化した

--- main.go
+++ main.go
@@ -12,26 +12,32 @@ func exists(name string) bool {
 	return os.IsNotExist(err)
 }
 
+func output(fileName...string) {
+	if len(fileName) == 0 {
+		return
+	}
+	if exists(fileName[0]) {
+		errString := fmt.Sprintf("cat: %s: No such file or directory\n", fileName)
+		fmt.Fprint(os.Stdout, errString)
+	}
+	// ファイルを開く
+	file, _ := os.Open(fileName[0])
+	defer file.Close()
+	// ファイル内のテキストを改行ごとに表示
+	fileBuf := bufio.NewScanner(file)
+	for i := 1; fileBuf.Scan(); i++ {
+		line := fileBuf.Text()
+		fmt.Println(line)
+	}
+}
+
 func main() {
 	flag.Parse()
 	args := flag.Args()
 	// ファイルが存在しなければ「cat: <ファイル名>: No such file or directory」と表示
 	for argIndex := 0; argIndex < len(args); argIndex++ {
 		fileName := args[argIndex]
-		if exists(fileName) {
-			errString := fmt.Sprintf("cat: %s: No such file or directory\n", fileName)
-			fmt.Fprint(os.Stdout, errString)
-			continue
-		}
-		// ファイルを開く
-		file, _ := os.Open(fileName)
-		defer file.Close()
-		// ファイル内のテキストを改行ごとに表示
-		fileBuf := bufio.NewScanner(file)
-		for i := 1; fileBuf.Scan(); i++ {
-			line := fileBuf.Text()
-			fmt.Println(line)
-		}
+		output(fileName)
 	}
 	if len(args) == 0 {
 		for {
