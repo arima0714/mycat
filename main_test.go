0206f986d83f29c81e5f911e97753512229f3ff1
@rima <32933855+arima0714@users.noreply.github.com>
2022-08-31 16:22:25 +0900

    テストを追加
    * ファイルから読む場合
    * 標準入力から読む場合

--- main_test.go
+++ main_test.go
@@ -1,12 +1,55 @@
 package main
 
 import (
+	"io/ioutil"
+	"log"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+func ExampleDoCat() {
+	// ファイルから読む場合
+	doCat("test/test00")
+	// Output:
+	// test01
+	// test02
+	// test03
+}
+
+func ExampleDoCat2() {
+	// 標準入力から読む場合
+	content := []byte("test1234")
+	tmpfile, err := ioutil.TempFile("", "example")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.Remove(tmpfile.Name())
+	if _, err := tmpfile.Write(content); err != nil {
+		log.Fatal(err)
+	}
+	if _, err := tmpfile.Seek(0, 0); err != nil {
+		log.Fatal(err)
+	}
+
+	oldStdin := os.Stdin
+	defer func() { os.Stdin = oldStdin }()
+	os.Stdin = tmpfile
+
+	doCat()
+
+	if err := tmpfile.Close(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Output:
+	// test1234
+
+}
+
 func Test_doCat(t *testing.T) {
+	// リダイレクトから読む場合
 
 }
 
