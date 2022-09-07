d254e3c3891be5d4f8a6ccee8617b6dd9a3f43a1
@rima <32933855+arima0714@users.noreply.github.com>
2022-09-07 14:41:02 +0900

    テストを現行のmain.goに沿うように修正

---
+++ export_test.go
@@ -0,0 +1,6 @@
+package main
+
+var (
+	DoCat    = doCat
+	Showends = showends
+)
--- main_test.go
+++ main_test.go
@@ -1,66 +1,28 @@
 package main_test
 
 import (
-	"io/ioutil"
-	"log"
-	"os"
-	"testing"
+	"bytes"
 
-	"github.com/stretchr/testify/assert"
+	"github.com/arima0714/mycat"
 )
 
 func ExampleDoCat() {
-	// ファイルから読む場合
-	doCat("test/test00")
-	// Output:
-	// test01
-	// test02
-	// test03
-}
+	var stdin bytes.Buffer
+	stdin.Write([]byte("test1234"))
 
-func ExampleDoCat2() {
-	// 標準入力から読む場合
-	content := []byte("test1234")
-	tmpfile, err := ioutil.TempFile("", "example")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer os.Remove(tmpfile.Name())
-	if _, err := tmpfile.Write(content); err != nil {
-		log.Fatal(err)
-	}
-	if _, err := tmpfile.Seek(0, 0); err != nil {
-		log.Fatal(err)
-	}
-
-	oldStdin := os.Stdin
-	defer func() { os.Stdin = oldStdin }()
-	os.Stdin = tmpfile
-
-	doCat()
-
-	if err := tmpfile.Close(); err != nil {
-		log.Fatal(err)
-	}
+	main.DoCat(&stdin)
 
 	// Output:
 	// test1234
-
 }
 
-func TestprocessLine(t *testing.T) {
-	*showends = true
-	line := "line"
-	var expected string = "line$"
-	var actual string
-	processLine(&line)
-	actual = line
-	assert.Equal(t, expected, actual)
+func ExampleDoCat2() {
+	var stdin bytes.Buffer
+	*main.Showends = true
+	stdin.Write([]byte("test1234"))
 
-	*showends = false
-	line = "line"
-	expected = "line"
-	processLine(&line)
-	actual = line
-	assert.Equal(t, expected, actual)
+	main.DoCat(&stdin)
+
+	// Output:
+	// test1234$
 }
