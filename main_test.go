9defca8c70bf62e9b704f2b4115c7876cae84520
@rima <32933855+arima0714@users.noreply.github.com>
2022-09-11 08:25:08 +0000

    Exportを頭に付けるように修正

--- main_test.go
+++ main_test.go
@@ -9,7 +9,7 @@ func ExampleDoCat() {
 	var stdin bytes.Buffer
 	stdin.Write([]byte("test1234"))
 
-	main.DoCat(&stdin)
+	main.Export_doCat(&stdin)
 
 	// Output:
 	// test1234
@@ -17,23 +17,23 @@ func ExampleDoCat() {
 
 func ExampleDoCat2() {
 	var stdin bytes.Buffer
-	*main.Showends = true
+	*main.Export_showends = true
 	stdin.Write([]byte("test1234\n1234test\n1t2e3s4t\n"))
 
-	main.DoCat(&stdin)
+	main.Export_doCat(&stdin)
 
 	// Output:
 	// test1234$
 	// 1234test$
 	// 1t2e3s4t$
-	*main.Showends = false
+	*main.Export_showends = false
 }
 
 func ExampleDoCat3() {
 	var stdin bytes.Buffer
-	*main.Showends = true
+	*main.Export_showends = true
 	stdin.Write([]byte("test01\ntest02\ntest03"))
-	main.DoCat(&stdin)
+	main.Export_doCat(&stdin)
 
 	// Output:
 	// test01$
