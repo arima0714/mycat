4763b1b3ecb0ffa294c8cb0b1604c7add71f84d4
@rima <32933855+arima0714@users.noreply.github.com>
2022-10-04 14:08:37 +0000

    ミーティングの反映

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ var (
 	showends = kingpin.Flag("show-ends", "show \"$\" end of line").Short('E').Bool()
 )
 
-func checkEoL(r rune) bool {
+func checkEOL(r rune) bool {
 	return *showends && r == '\n'
 }
 
@@ -29,7 +29,7 @@ func doCat(f io.Reader) {
 			}
 			log.Fatal(err)
 		}
-		if checkEoL(r) {
+		if checkEOL(r) {
 			fmt.Print("$")
 		}
 		fmt.Print(string(r))
