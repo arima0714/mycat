f214552cb08b0d16145bf09c6a462d83b468125f
@rima <32933855+arima0714@users.noreply.github.com>
2022-10-07 14:04:38 +0000

    会議の反映

--- export_test.go
+++ export_test.go
@@ -3,4 +3,5 @@ package main
 var (
 	ExpDoCat    = doCat
 	ExpShowends = showends
+	ExpCheckEOL = checkEOL
 )
--- main_test.go
+++ main_test.go
@@ -2,56 +2,48 @@ package main_test
 
 import (
 	"bytes"
+	"testing"
 
 	"github.com/arima0714/mycat"
+	"github.com/stretchr/testify/assert"
 )
 
-func ExampleDoCat() {
-	var f bytes.Buffer
+func resetShowends() {
 	*main.ExpShowends = false
-	f.Write([]byte("test1\ntest2\ntest3"))
+}
 
+func ExampleExpDoCat() {
+	defer resetShowends()
+	
+	var f bytes.Buffer
+	f.Write([]byte("test1\ntest2\ntest3\n"))
+	main.ExpDoCat(&f)
+
+	*main.ExpShowends = true
+	f.Write([]byte("あいう１\nあいう２\nあいう３"))
 	main.ExpDoCat(&f)
 
 	// Output:
 	// test1
 	// test2
 	// test3
-}
-
-func ExampleDoCat2() {
-	var f bytes.Buffer
-	*main.ExpShowends = true
-	f.Write([]byte("test1\ntest2\ntest3\n"))
-
-	main.ExpDoCat(&f)
-
-	// Output:
-	// test1$
-	// test2$
-	// test3$
-}
-
-func ExampleDoCat3() {
-	var f bytes.Buffer
-	*main.ExpShowends = true
-	f.Write([]byte("test1\ntest2\ntest3"))
-	main.ExpDoCat(&f)
-
-	// Output:
-	// test1$
-	// test2$
-	// test3
-}
-
-func ExampleDoCat4() {
-	var f bytes.Buffer
-	*main.ExpShowends = true
-	f.Write([]byte("あいう１\nあいう２\nあいう３"))
-	main.ExpDoCat(&f)
-
-	// Output:
 	// あいう１$
 	// あいう２$
 	// あいう３
+}
+
+func TestCheckEOL(t *testing.T) {
+	defer resetShowends()
+
+	*main.ExpShowends = false
+	assert.Equal(t, main.ExpCheckEOL('a'), false)
+
+	*main.ExpShowends = false
+	assert.Equal(t, main.ExpCheckEOL('\n'), false)
+	
+	*main.ExpShowends = true
+	assert.Equal(t, main.ExpCheckEOL('a'), false)
+	
+	*main.ExpShowends = true
+	assert.Equal(t, main.ExpCheckEOL('\n'), true)
 }
